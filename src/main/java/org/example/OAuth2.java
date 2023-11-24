88619d2a27856431e083ed4a7600f4af23a2dc36
CristianSw <cristian.dolinta@tutanota.com>
2023-11-24 15:52:24 +0200

    Added selenium dependencies to pom.xml and provided OAuth2 code automatization

---
+++ src/main/java/org/example/OAuth2.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static io.restassured.RestAssured.*;
+
+public class OAuth2 {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new FirefoxDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&auth_url=https://accounts.google.com/o/oauth2/v2/auth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https://rahulshettyacademy.com/getCourse.php&state=verifyfjdss");
+        driver.findElement(By.id("identifierId")).sendKeys("foroneplusone.a00001");
+        driver.findElement(By.id("identifierId")).sendKeys(Keys.ENTER);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='password']")));
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("qazwsxgh32umym");
+        driver.findElement(By.cssSelector(".whsOnd")).sendKeys(Keys.ENTER);
+        wait.until(ExpectedConditions.urlContains("rahulshettyacademy.com/getCourse.php"));
+
+        String url = driver.getCurrentUrl();
+        driver.close();
+        String[] splitCode = url.split("code=");
+        String actualCode = splitCode[1].split("&scope=")[0];
+        System.out.println(actualCode);
+
+
+
+        String getAuthTokenResponse = given()
+                .urlEncodingEnabled(false)
+                .queryParam("code", actualCode)
+                .queryParam("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+                .queryParam("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
+                .queryParam("redirect_uri", "https://rahulshettyacademy.com/getCourse.php")
+                .queryParam("grant_type", "authorization_code")
+                .when().post("https://www.googleapis.com/oauth2/v4/token")
+                .then().assertThat().statusCode(200).extract().response().asString();
+
+        String accessToken = UtilitiesMain.getValueFromJsonString(getAuthTokenResponse,"access_token");
+
+        String getBookResponse = given()
+                .queryParam("access_token", accessToken)
+                .when().get("https://rahulshettyacademy.com/getCourse.php")
+                .then().log().all().assertThat().statusCode(200).extract().response().asString();
+//        System.out.println(getBookResponse);
+    }
+}
